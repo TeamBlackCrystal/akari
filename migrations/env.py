1e406f5b6ec7cd62a714b09ef7275ee901e7f3c2
yupix <yupi0982@outlook.jp>
2023-03-22 02:20:31 +0900

    chore: target_metadatasにreminderを追加

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.domain.models.shared.user import User, Strike
+from src.domain.models.shared.user import Reminder, User, Strike
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadatas = [User.metadata, Strike.metadata]
+target_metadatas = [User.metadata, Reminder.metadata, Strike.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
