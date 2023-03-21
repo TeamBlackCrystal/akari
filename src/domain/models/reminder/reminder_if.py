37e39bb349d412ff82d393bed373a004b1aa051b
yupix <yupi0982@outlook.jp>
2023-03-22 02:21:14 +0900

    feat: get_not_done_listsメソッドを作成

--- src/domain/models/reminder/reminder_if.py
+++ src/domain/models/reminder/reminder_if.py
@@ -5,10 +5,13 @@ from typing import TYPE_CHECKING
 
 
 if TYPE_CHECKING:
-    from src.domain.models.shared.user import Reminder, User
+    from src.domain.models.shared.user import Reminder
 
 
 class IFReminderRepository(ABC):
+    async def get_not_done_lists(self) -> list[Reminder]:
+        ...
+
     async def get_lists(self, user_id: str) -> list[Reminder]:
         ...
 
--- src/domain/models/reminder/reminder_repository.py
+++ src/domain/models/reminder/reminder_repository.py
@@ -5,16 +5,24 @@ from src.db import session
 
 
 class ReminderRepository(IFReminderRepository):
+    async def get_not_done_lists(self) -> list[Reminder]:
+        async with session() as _session:
+            async with _session.begin():
+                search_reminder = await _session.execute(
+                    select(Reminder).where(Reminder.is_done.is_(False))
+                )
+        return [i for i in search_reminder.scalars()]
+
     async def get_lists(self, user_id: str) -> list[Reminder]:
         async with session() as _session:
             async with _session.begin():
                 search_reminder = await _session.execute(
-                    select(Reminder).where(Reminder.user == user_id)
+                    select(Reminder).where(Reminder.user_id == user_id)
                 )
         return [i for i in search_reminder.scalars()]
 
     async def create(self, title: str, user_id: str, note_id: str) -> Reminder:
-        create_reminder = Reminder(title=title, user=user_id, note_id=note_id)
+        create_reminder = Reminder(title=title, user_id=user_id, note_id=note_id)
         async with session() as _session:
             async with _session.begin():
                 _session.add(create_reminder)
