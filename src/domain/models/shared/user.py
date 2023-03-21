22af4e9a7d1c7977f619bd1cb0f90be0a73c9476
yupix <yupi0982@outlook.jp>
2023-03-22 02:19:54 +0900

    refactor: データベースのモデルを再定義

--- src/domain/models/shared/user.py
+++ src/domain/models/shared/user.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from datetime import datetime
 import uuid
-from sqlalchemy import Boolean, Date, ForeignKey, String
+from sqlalchemy import Boolean, DateTime, ForeignKey, String
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from src.db import Base
@@ -10,28 +10,33 @@ from src.db import Base
 
 class User(Base):
     __tablename__ = 'users'
-    id: Mapped[str] = mapped_column(String, default=str(uuid.uuid4()), primary_key=True)
-    misskey_id: Mapped[str] = mapped_column(String, unique=True)
-    created_at: Mapped[datetime] = mapped_column(Date, default=datetime.utcnow)
-    strike: Mapped[list[Strike]] = relationship()
-    reminder: Mapped[list[Reminder]] = relationship()
+    misskey_id: Mapped[str] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+
+    strikes: Mapped[list["Strike"]] = relationship(back_populates='user')
+    reminders: Mapped[list["Reminder"]] = relationship(back_populates='user')
+
 
 
 class Strike(Base):
     __tablename__ = 'strikes'
 
-    id: Mapped[str] = mapped_column(String, default=str(uuid.uuid4()), primary_key=True)
-    user: Mapped[User] = mapped_column(ForeignKey('users.id'))
-    reason: Mapped[str] = mapped_column(String)
-    created_at: Mapped[datetime] = mapped_column(Date, default=datetime.utcnow)
+    id: Mapped[str] = mapped_column(default=str(uuid.uuid4()), primary_key=True)
+    user_id: Mapped[str] = mapped_column(ForeignKey('users.misskey_id'))
+    user: Mapped[User] = relationship(back_populates='strikes')
+    reason: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow())
 
 
 class Reminder(Base):
     __tablename__ = 'reminders'
 
-    id: Mapped[str] = mapped_column(String, default=str(uuid.uuid4()), primary_key=True)
-    user: Mapped[User] = mapped_column(ForeignKey('users.id'))
-    title: Mapped[str] = mapped_column(String)
-    note_id: Mapped[str] = mapped_column(String, unique=True)
-    is_done: Mapped[bool] = mapped_column(Boolean, default=False)
-    created_at: Mapped[datetime] = mapped_column(Date, default=datetime.utcnow)
+    id: Mapped[str] = mapped_column(default=str(uuid.uuid4()), primary_key=True)
+
+    user_id: Mapped[str] = mapped_column(ForeignKey("users.misskey_id"))
+    user: Mapped["User"] = relationship(back_populates="reminders")
+
+    title: Mapped[str] = mapped_column()
+    note_id: Mapped[str] = mapped_column(unique=True)
+    is_done: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow())
