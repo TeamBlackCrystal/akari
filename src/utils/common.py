9637adcca01417e0e967e993c41f4a687c8de21b
yupix <yupi0982@outlook.jp>
2023-04-20 10:46:30 +0000

    chore: batcher関数を追加

--- src/utils/common.py
+++ src/utils/common.py
@@ -3,3 +3,14 @@ from mipac.models import LiteUser
 
 def get_name(user: LiteUser):
     return user.nickname if user.nickname else user.username
+
+async def batcher(iterable, n):
+    count = 0
+    result = []
+    async for i in iterable:
+        if n == count:
+            break
+        count = count+1
+        result.append(i.decode('utf-8'))
+        
+    return result
