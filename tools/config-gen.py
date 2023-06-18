ff134a84d212bba6ae398c7ff4db5002f37f02aa
yupix <yupi0982@outlook.jp>
2023-06-18 11:54:49 +0900

    feat: configの値の変え方がよくないのを修正

--- tools/config-gen.py
+++ tools/config-gen.py
@@ -12,10 +12,9 @@ BOT_OWNERS = os.environ.get('BOT_OWNERS')
 if BOT_TOKEN is None or BOT_URL is None or BOT_OWNERS is None:
     raise ValueError('BOT_TOKEN, BOT_URL, BOT_OWNERSのいずれかが定義されていません')
 raw_config = config.__dict__['_sections']
-print(raw_config)
-raw_config['BOT']['token'] = BOT_TOKEN
-raw_config['BOT']['url'] = BOT_URL
-raw_config['BOT']['owner_ids'] = json.loads(BOT_OWNERS)
+config.set('BOT', 'token', BOT_TOKEN)
+config.set('BOT', 'url', BOT_URL)
+config.set('BOT', 'owner_ids', BOT_OWNERS)
 
 with open('./config.ini', mode='w', encoding='utf-8') as f:
     config.write(f)
